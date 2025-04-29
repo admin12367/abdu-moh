7b1587f8f9f80be7e740af8bc0a419643090912f
admin12367 <mohammedabdurahman051@gmail.com>
2025-04-29 19:11:37 +0300

    first commit

---
+++ components/ContactForm.tsx
@@ -0,0 +1,65 @@
+"use client"
+import React from "react";
+
+const ContactForm = () => {
+  
+
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event: any) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "a4744bf8-bdcc-4945-af53-94dc8e8ed84f");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h2 className="text-2xl font-bold mb-5 text-white">Contact Me</h2>
+      <div className="mb-3">
+        <input
+          type="text"
+          placeholder="Your name"
+          name="name"
+          className="w-full px-3 py-2 text-sm text-black-200 placeholder-gray-400 bg-white border-0 rounded shadow"
+        />
+      </div>
+      <div className="mb-3">
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          className="w-full px-3 py-2 text-sm text-black-200 placeholder-gray-400 bg-white border-0 rounded shadow"
+        />
+      </div>
+      <div className="mb-3">
+        <input
+          placeholder="Your message"
+          name="message"
+          className="w-full px-3 py-2 text-sm text-black-200 placeholder-gray-400 bg-white border-0 rounded shadow"
+        />
+      </div>
+      <button className="px-6 mb-10 py-3 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear bg-blue-500 hover:bg-blue-400">
+        Send a message
+      </button>
+    </form>
+  );
+};
+
+export default ContactForm;
---
+++ constants/index.ts
@@ -0,0 +1,149 @@
+import { link } from "fs";
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+
+export const SkillData = [
+  {
+    name: "Html 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Css",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "JavaScript",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Redux",
+    Image: "/redux.png",
+    width: 80,
+    height: 80,
+  },
+
+  {
+    name: "TypeScript",
+    Image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Next js 13",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Framer Motion",
+    Image: "/bootstrap.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Stripe Payment",
+    Image: "/react-2.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Node js",
+    Image: "/node-js.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Mongo db",
+    Image: "/nextjs.png",
+    width: 80,
+    height: 80,
+  },
+];
+
+export const Socials = [
+  {
+    name: "github",
+    src: "/github.svg",
+    link: "https://github.com/admin12367/"
+  },
+  {
+    name: "twitter",
+    src: "/twitter.svg",
+    link: "https://x.com/abdu60491/"
+  },
+  {
+    name: "Linkedin",
+    src: "/linkedin.svg",
+    link: "https://www.linkedin.com/in/abdurahman-mohammed-195602319/",
+  },
+];
+export const Projects = [
+  {
+    title: "",
+    text: "",
+    src: "/edusity.jpeg",
+    link: "https://admin12367.github.io/edu.college/"
+  },
+  {
+    title: "",
+    text: "",
+    src: "/ecommerce.jpeg",
+    link: "https://admin12367.github.io/ecommerce/"
+  },
+  {
+    title: "",
+    text: "",
+    src: "/WebPortfolio.png",
+    link: "/"
+  },
+  {
+    title: "",
+    text: "",
+    src: "/restaurant.jpeg",
+    link: "https://admin12367.github.io/restaurant/"
+  },
+];
+
+export const NavLinks = [
+  {
+    name: "/",
+    icon: RxHome,
+    link: "/",
+  },
+
+  {
+    name: "/my-skills",
+    icon: RxDashboard,
+    link: "/my-skills",
+  },
+
+  {
+    name: "/my-projects",
+    icon: RxClipboard,
+    link: "/my-projects",
+  },
+
+  {
+    name: "/contact-me",
+    icon: RxPerson,
+    link: "/contact-me",
+  },
+   
+];
