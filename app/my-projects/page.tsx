bdbbc38f19a15ca4424a801afb2d476e98718690
admin12367 <mohammedabdurahman051@gmail.com>
2025-04-30 02:56:58 +0300

    first commit

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -1,19 +1,19 @@
 
-"use client";
-import Link from 'next/link';
-import ProjectCard from '@/components/ProjectCard';
-import { Projects } from '@/constants';
-import React from 'react';
+"use client"
+
+import ProjectCard from '@/components/ProjectCard'
+import { Projects } from '@/constants'
+import Link from 'next/link'
+import React from 'react'
 
 const Page = () => {
   return (
     <div
-      style={{ backgroundImage: "url(/mountains.jpg)" }}
-      className='w-screen h-screen flex items-center justify-center bg-center bg-cover'
-    >
+    style={{backgroundImage: "url(/bg-3.jpg)"}}
+    className='w-screen h-screen flex items-center justify-center bg-center bg-cover'>
       <div className='grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%]'>
         {Projects.map((project) => (
-          <Link key={project.id} href={project.link}> {/* Use a unique identifier for key */}
+          <Link key={project.id} href={project.link}>
             <ProjectCard
               title={project.title}
               text={project.text}
